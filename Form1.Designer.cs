dcb06d0c96ea6fa2c0ec00569e79136a851b6188
SpwaN <kevin.berling@hotmail.com>
2022-12-05 12:33:07 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace DVGB07_LABB2_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblLottoRad = new System.Windows.Forms.Label();
+            this.lblDragningar = new System.Windows.Forms.Label();
+            this.lbl5Ratt = new System.Windows.Forms.Label();
+            this.lbl6Ratt = new System.Windows.Forms.Label();
+            this.lbl7Ratt = new System.Windows.Forms.Label();
+            this.txbValue1 = new System.Windows.Forms.TextBox();
+            this.txbValue7 = new System.Windows.Forms.TextBox();
+            this.txbValue6 = new System.Windows.Forms.TextBox();
+            this.txbValue5 = new System.Windows.Forms.TextBox();
+            this.txbValue4 = new System.Windows.Forms.TextBox();
+            this.txbValue3 = new System.Windows.Forms.TextBox();
+            this.txbValue2 = new System.Windows.Forms.TextBox();
+            this.txbDraggning = new System.Windows.Forms.TextBox();
+            this.txb5Ratt = new System.Windows.Forms.TextBox();
+            this.txb6Ratt = new System.Windows.Forms.TextBox();
+            this.txb7Ratt = new System.Windows.Forms.TextBox();
+            this.lblErrorMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(549, 62);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(149, 40);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Starta Lotto";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblLottoRad
+            // 
+            this.lblLottoRad.AutoSize = true;
+            this.lblLottoRad.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLottoRad.Location = new System.Drawing.Point(13, 21);
+            this.lblLottoRad.Name = "lblLottoRad";
+            this.lblLottoRad.Size = new System.Drawing.Size(113, 20);
+            this.lblLottoRad.TabIndex = 1;
+            this.lblLottoRad.Text = "Din lottorad: ";
+            // 
+            // lblDragningar
+            // 
+            this.lblDragningar.AutoSize = true;
+            this.lblDragningar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDragningar.Location = new System.Drawing.Point(121, 72);
+            this.lblDragningar.Name = "lblDragningar";
+            this.lblDragningar.Size = new System.Drawing.Size(147, 20);
+            this.lblDragningar.TabIndex = 2;
+            this.lblDragningar.Text = "Antal dragningar:";
+            // 
+            // lbl5Ratt
+            // 
+            this.lbl5Ratt.AutoSize = true;
+            this.lbl5Ratt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5Ratt.Location = new System.Drawing.Point(146, 152);
+            this.lbl5Ratt.Name = "lbl5Ratt";
+            this.lbl5Ratt.Size = new System.Drawing.Size(64, 20);
+            this.lbl5Ratt.TabIndex = 3;
+            this.lbl5Ratt.Text = "5 Rätt:";
+            // 
+            // lbl6Ratt
+            // 
+            this.lbl6Ratt.AutoSize = true;
+            this.lbl6Ratt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl6Ratt.Location = new System.Drawing.Point(322, 152);
+            this.lbl6Ratt.Name = "lbl6Ratt";
+            this.lbl6Ratt.Size = new System.Drawing.Size(64, 20);
+            this.lbl6Ratt.TabIndex = 4;
+            this.lbl6Ratt.Text = "6 Rätt:";
+            // 
+            // lbl7Ratt
+            // 
+            this.lbl7Ratt.AutoSize = true;
+            this.lbl7Ratt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl7Ratt.Location = new System.Drawing.Point(498, 152);
+            this.lbl7Ratt.Name = "lbl7Ratt";
+            this.lbl7Ratt.Size = new System.Drawing.Size(64, 20);
+            this.lbl7Ratt.TabIndex = 5;
+            this.lbl7Ratt.Text = "7 Rätt:";
+            // 
+            // txbValue1
+            // 
+            this.txbValue1.Location = new System.Drawing.Point(125, 21);
+            this.txbValue1.Name = "txbValue1";
+            this.txbValue1.Size = new System.Drawing.Size(100, 26);
+            this.txbValue1.TabIndex = 6;
+            this.txbValue1.TextChanged += new System.EventHandler(this.txbValue1_TextChanged);
+            // 
+            // txbValue7
+            // 
+            this.txbValue7.Location = new System.Drawing.Point(761, 21);
+            this.txbValue7.Name = "txbValue7";
+            this.txbValue7.Size = new System.Drawing.Size(100, 26);
+            this.txbValue7.TabIndex = 12;
+            this.txbValue7.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txbValue6
+            // 
+            this.txbValue6.Location = new System.Drawing.Point(655, 21);
+            this.txbValue6.Name = "txbValue6";
+            this.txbValue6.Size = new System.Drawing.Size(100, 26);
+            this.txbValue6.TabIndex = 11;
+            // 
+            // txbValue5
+            // 
+            this.txbValue5.Location = new System.Drawing.Point(549, 21);
+            this.txbValue5.Name = "txbValue5";
+            this.txbValue5.Size = new System.Drawing.Size(100, 26);
+            this.txbValue5.TabIndex = 10;
+            // 
+            // txbValue4
+            // 
+            this.txbValue4.Location = new System.Drawing.Point(443, 21);
+            this.txbValue4.Name = "txbValue4";
+            this.txbValue4.Size = new System.Drawing.Size(100, 26);
+            this.txbValue4.TabIndex = 9;
+            // 
+            // txbValue3
+            // 
+            this.txbValue3.Location = new System.Drawing.Point(337, 21);
+            this.txbValue3.Name = "txbValue3";
+            this.txbValue3.Size = new System.Drawing.Size(100, 26);
+            this.txbValue3.TabIndex = 8;
+            // 
+            // txbValue2
+            // 
+            this.txbValue2.Location = new System.Drawing.Point(231, 21);
+            this.txbValue2.Name = "txbValue2";
+            this.txbValue2.Size = new System.Drawing.Size(100, 26);
+            this.txbValue2.TabIndex = 7;
+            // 
+            // txbDraggning
+            // 
+            this.txbDraggning.Location = new System.Drawing.Point(274, 69);
+            this.txbDraggning.Name = "txbDraggning";
+            this.txbDraggning.Size = new System.Drawing.Size(269, 26);
+            this.txbDraggning.TabIndex = 13;
+            this.txbDraggning.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // txb5Ratt
+            // 
+            this.txb5Ratt.Location = new System.Drawing.Point(216, 149);
+            this.txb5Ratt.Name = "txb5Ratt";
+            this.txb5Ratt.Size = new System.Drawing.Size(100, 26);
+            this.txb5Ratt.TabIndex = 14;
+            this.txb5Ratt.Text = "0";
+            // 
+            // txb6Ratt
+            // 
+            this.txb6Ratt.Location = new System.Drawing.Point(392, 152);
+            this.txb6Ratt.Name = "txb6Ratt";
+            this.txb6Ratt.Size = new System.Drawing.Size(100, 26);
+            this.txb6Ratt.TabIndex = 15;
+            this.txb6Ratt.Text = "0";
+            // 
+            // txb7Ratt
+            // 
+            this.txb7Ratt.Location = new System.Drawing.Point(568, 152);
+            this.txb7Ratt.Name = "txb7Ratt";
+            this.txb7Ratt.Size = new System.Drawing.Size(100, 26);
+            this.txb7Ratt.TabIndex = 16;
+            this.txb7Ratt.Text = "0";
+            // 
+            // lblErrorMsg
+            // 
+            this.lblErrorMsg.AutoSize = true;
+            this.lblErrorMsg.Location = new System.Drawing.Point(270, 114);
+            this.lblErrorMsg.Name = "lblErrorMsg";
+            this.lblErrorMsg.Size = new System.Drawing.Size(0, 20);
+            this.lblErrorMsg.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 202);
+            this.Controls.Add(this.lblErrorMsg);
+            this.Controls.Add(this.txb7Ratt);
+            this.Controls.Add(this.txb6Ratt);
+            this.Controls.Add(this.txb5Ratt);
+            this.Controls.Add(this.txbDraggning);
+            this.Controls.Add(this.txbValue2);
+            this.Controls.Add(this.txbValue3);
+            this.Controls.Add(this.txbValue4);
+            this.Controls.Add(this.txbValue5);
+            this.Controls.Add(this.txbValue6);
+            this.Controls.Add(this.txbValue7);
+            this.Controls.Add(this.txbValue1);
+            this.Controls.Add(this.lbl7Ratt);
+            this.Controls.Add(this.lbl6Ratt);
+            this.Controls.Add(this.lbl5Ratt);
+            this.Controls.Add(this.lblDragningar);
+            this.Controls.Add(this.lblLottoRad);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Lotto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblLottoRad;
+        private System.Windows.Forms.Label lblDragningar;
+        private System.Windows.Forms.Label lbl5Ratt;
+        private System.Windows.Forms.Label lbl6Ratt;
+        private System.Windows.Forms.Label lbl7Ratt;
+        private System.Windows.Forms.TextBox txbValue1;
+        private System.Windows.Forms.TextBox txbValue7;
+        private System.Windows.Forms.TextBox txbValue6;
+        private System.Windows.Forms.TextBox txbValue5;
+        private System.Windows.Forms.TextBox txbValue4;
+        private System.Windows.Forms.TextBox txbValue3;
+        private System.Windows.Forms.TextBox txbValue2;
+        private System.Windows.Forms.TextBox txbDraggning;
+        private System.Windows.Forms.TextBox txb5Ratt;
+        private System.Windows.Forms.TextBox txb6Ratt;
+        private System.Windows.Forms.TextBox txb7Ratt;
+        private System.Windows.Forms.Label lblErrorMsg;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVGB07_LABB2_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int maxValue = 36;
+            int minValue = 0;
+            if(int.Parse(txbValue1.Text) >= maxValue || int.Parse(txbValue1.Text) <= minValue
+                || int.Parse(txbValue2.Text) >= maxValue || int.Parse(txbValue2.Text) <= minValue
+                || int.Parse(txbValue3.Text) >= maxValue || int.Parse(txbValue3.Text) <= minValue
+                || int.Parse(txbValue4.Text) >= maxValue || int.Parse(txbValue4.Text) <= minValue
+                || int.Parse(txbValue5.Text) >= maxValue || int.Parse(txbValue5.Text) <= minValue
+                || int.Parse(txbValue6.Text) >= maxValue || int.Parse(txbValue6.Text) <= minValue
+                || int.Parse(txbValue7.Text) >= maxValue || int.Parse(txbValue7.Text) <= minValue)
+            {
+                lblErrorMsg.Text = "ERROR! Ha inga värden över 35 eller under 0!";
+                return;
+            } else
+            {
+                int[] tal = new int[7];
+
+                tal[0] = int.Parse(txbValue1.Text);
+                tal[1] = int.Parse(txbValue2.Text);
+                tal[2] = int.Parse(txbValue3.Text);
+                tal[3] = int.Parse(txbValue4.Text);
+                tal[4] = int.Parse(txbValue5.Text);
+                tal[5] = int.Parse(txbValue6.Text);
+                tal[6] = int.Parse(txbValue7.Text);
+
+                Array.Sort(tal);
+
+                if (tal.Distinct().Count() != tal.Count())
+                {
+                    lblErrorMsg.Text = "ERROR! Du har angett dubbletter";
+                    return;
+                }
+
+                Random rand = new Random();
+                int[] number = new int[7];
+                int AntalDragningar = int.Parse(txbDraggning.Text);
+                while (AntalDragningar != 0)
+                {
+                    int Antal = 0;
+                    for (int i = 0; i < 7; i++)
+                    {
+                        int rndTal = rand.Next(1, 35);
+                        if (number.Contains(rndTal))
+                        {
+                            i--;
+                        }
+                        else
+                        {
+                            number[i] = rndTal;
+                        }
+                    }
+                    for(int i = 0; i<7; i++)
+                    {
+                        if (number.Contains(tal[i])) { Antal++; }
+                    }
+                    if (Antal == 5)
+                    {
+                        int tmpTal5 = int.Parse(txb5Ratt.Text);
+                        tmpTal5++;
+                        txb5Ratt.Text = tmpTal5.ToString();
+                    } else if (Antal == 6)
+                    {
+                        int tmpTal6 = int.Parse(txb6Ratt.Text);
+                        tmpTal6++;
+                        txb6Ratt.Text = tmpTal6.ToString();
+                    } else if (Antal == 7)
+                    {
+                        int tmpTal7 = int.Parse(txb7Ratt.Text);
+                        tmpTal7++;
+                        txb7Ratt.Text = tmpTal7.ToString();
+                    }
+
+                    AntalDragningar--;
+                }
+                    Array.Sort(number);
+
+
+                lblErrorMsg.Text = "Din lottorad: " + string.Join("-", tal);
+            }
+        }
+
+        private void txbValue1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
